88a9dec9cc81873a00af1d4e25d53cdb7a0d41de
coderSaurav346 <r94381880@gmail.com>
2024-06-14 10:07:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 const inp = document.querySelector(".inp")
-const img = document.querySelector(".img");
+let img = document.querySelector(".img");
 document.addEventListener('DOMContentLoaded', (event) => {
-  const savedImage = localStorage.getItem("imgFile");
-if (savedImage) {
-  img.src = savedImage;
-  img.style.opacity = 1;
-} else {
-  img.src = "/test/defaultImg.png";
-  img.style.opacity = 0.5;
-}
-})
-
-inp.addEventListener("change", (e) => {
-  img.style.opacity = 1;
-  let file = e.target.files[0];
-  if (file) {
-    if (file.type.startsWith("image/")) {
-      let filePath = URL.createObjectURL(inp.files[0]);
-      img.src = filePath;
-      localStorage.setItem("imgFile", filePath);
-    }
-    else {
-      alert('Please upload an image file.');
-      img.src = "/test/defaultImg.png";
-      img.style.opacity = 0.5;
-    }
+  
+  const savedImage = localStorage.getItem('profileImage');
+  let image = document.querySelector(".img");
+  if (savedImage) {
+    image.src = savedImage;
+    image.style.opacity = 1;
+  } else {
+    image.src = '//test/defaultImg.png'; // Set a default image
   }
 })
 
+  inp.addEventListener("change", (e) => {
+    img.style.opacity = 1;
+    let file = e.target.files[0];
+    if (file) {
+      if (file.type.startsWith("image/")) {
+        let filePath = URL.createObjectURL(inp.files[0]);
+        localStorage.setItem("profileImage", filePath);
+        img.src = localStorage.getItem("profileImage");
+      }
+      else {
+        alert('Please upload an image file.');
+        img.src = "/test/defaultImg.png";
+        img.style.opacity = 0.5;
+      }
+    }
+  })
+
 function handleImageError(image) {
-  
-  img.src = "defaultImg.png";
+  img.src = "/test/defaultImg.png";
   img.style.opacity = 0.5;
 }
